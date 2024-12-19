ced50491c19a5092faa3c57f002be7f105973db6
Adeola Samuel <meetprecious1403@gmail.com>
2024-12-19 15:41:48 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Directly')
