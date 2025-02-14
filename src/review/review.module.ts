b3fbb276de961d3c591a37871d167783fd98110e
Adeola Samuel <meetprecious1403@gmail.com>
2025-02-14 18:53:05 +0100

    update imports

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -4,9 +4,10 @@ import { Review } from './entities/review.entity';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
 import { Service } from 'src/service/entities/service.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review, Service])],
+  imports: [TypeOrmModule.forFeature([Review, Service]), UsersModule],
   controllers: [ReviewController],
   providers: [ReviewService],
 })
